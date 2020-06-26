124d41c5bf98862cb4d966cb174d376b41552c2d
Thang <58561617+Thangnguyen11@users.noreply.github.com>
2020-06-26 20:43:39 +0700

    Done Add UI

--- StudentManagement/StudentManagement/View/LoginWindow.xaml.cs
+++ StudentManagement/StudentManagement/View/LoginWindow.xaml.cs
@@ -1,7 +1,6 @@
-﻿using DTO;
-using StudentManagement.API;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,33 +11,45 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-
-namespace StudentManagement.View
+namespace StudentManagement
 {
     /// <summary>
-    /// Interaction logic for LoginWindow.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class LoginWindow : Window
     {
+        //T luu cai nay lai ne, dung dc thi dung kh thi xoa dum`
+        string userId, userPw;
+
         public LoginWindow()
         {
             InitializeComponent();
         }
-        private void MoveLoginWindow(object sender, MouseButtonEventArgs e)
-        {
-            if(e.LeftButton == MouseButtonState.Pressed)
-            {
-                DragMove();
-            }
-        }
-        private async void LoginButton_Click(object sender, RoutedEventArgs e)
-        {
-            Account user = new Account(Username.Text,Password.Password.ToString());
 
-            Task<Login> accountResponse = Controller.Controller.Instance.Login(user);
-            await accountResponse;
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            RegisterWindow rwd = new RegisterWindow();
+            rwd.ShowDialog();
+        }
+
+        private void btnShutdown_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Luu id va password
+            userId = txbUserId.Text;
+            txbUserId.Text = "";
+            userPw = pwbPassword.Password;
+            pwbPassword.Password = "";
+            MenuWindow mwd = new MenuWindow();
+            mwd.Show();
+            this.Close();
         }
     }
 }
