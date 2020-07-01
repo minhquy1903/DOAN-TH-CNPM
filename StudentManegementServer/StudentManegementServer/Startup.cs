3aee730d6c439d3419a68e4e4967d656ea56f3d4
minhquy <minhquy1903@gmail.com>
2020-07-01 20:45:28 +0700

    fixbug

--- StudentManegementServer/StudentManegementServer/Startup.cs
+++ StudentManegementServer/StudentManegementServer/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -41,6 +42,8 @@ namespace StudentManegementServer
             app.UseRouting();
 
             app.UseAuthorization();
+
+
 
             app.UseEndpoints(endpoints =>
             {
